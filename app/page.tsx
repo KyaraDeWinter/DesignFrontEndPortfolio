736448296088f5967b6208448b1b7f4233d8e0fb
fabiantax87 <fabiantax87@users.noreply.github.com>
2024-06-13 16:14:01 +0200

    Fixed image imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,54 +2,73 @@
 import React from "react";
 import Card from "@/components/Card/Card";
 import chillOrbV1Image from "../public/images/chillorbV1.jpg";
-import Image from "next/image";
+import chillOrbV2Image from "../public/images/chillorbV2.jpg";
+import chillOrbV3Image from "../public/images/chillorbV3.jpg";
+import buildAtHomeV1Image from "../public/images/buildathomeV1.jpg";
+import buildAtHomeV2Image from "../public/images/buildathomeV2.jpg";
+import buildAtHomeV3Image from "../public/images/buildathomeV3.jpg";
+import buildAtHomeV4Image from "../public/images/buildathomeV4.jpg";
+import aquasenseV1Image from "../public/images/aquasenseV1.jpg";
+import aquasenseV2Image from "../public/images/aquasenseV2.jpg";
+import aquasenseV3Image from "../public/images/aquasenseV3.jpg";
+import nextgenV1Image from "../public/images/nextgenV1.jpg";
+import nextgenV2Image from "../public/images/nextgenV2.jpg";
+import nextgenV3Image from "../public/images/nextgenV3.jpg";
+import nextgenV4Image from "../public/images/nextgenV4.jpg";
+import nextgenV5Image from "../public/images/nextgenV5.jpg";
+import nextgenV6Image from "../public/images/nextgenV6.jpg";
+import rocV1Image from "../public/images/rocV1.jpg";
+import rocV2Image from "../public/images/rocV2.jpg";
+import flyerDuurzaamheidV1 from "../public/images/flyerDuurzaamheidV1.png";
+import flyerDuurzaamheidV2 from "../public/images/flyerDuurzaamheidV2.png";
+import buildathomeBolognaV2 from "../public/images/buildathomeBolognaV2.jpg";
+import posterDuurzaamheidV1 from "../public/images/posterDuurzaamheidV1.png";
 
 export default function Home() {
   const chillOrbImages = [
-    { href: "/images/chillorbV1.jpg", width: 500, height: 500 },
-    { href: "/images/chillorbV2.jpg", width: 500, height: 500 },
-    { href: "/images/chillorbV3.jpg", width: 500, height: 500 },
+    { src: chillOrbV1Image, width: 500, height: 500 },
+    { src: chillOrbV2Image, width: 500, height: 500 },
+    { src: chillOrbV3Image, width: 500, height: 500 },
   ];
 
   const buildAtHomeImages = [
-    { href: "/images/buildathomeV1.jpg", width: 500, height: 500 },
-    { href: "/images/buildathomeV2.jpg", width: 500, height: 500 },
-    { href: "/images/buildathomeV3.jpg", width: 500, height: 500 },
-    { href: "/images/buildathomeV4.jpg", width: 500, height: 500 },
+    { src: buildAtHomeV1Image, width: 500, height: 500 },
+    { src: buildAtHomeV2Image, width: 500, height: 500 },
+    { src: buildAtHomeV3Image, width: 500, height: 500 },
+    { src: buildAtHomeV4Image, width: 500, height: 500 },
   ];
 
   const aquaSenseImages = [
-    { href: "/images/aquasenseV1.jpg", width: 500, height: 500 },
-    { href: "/images/aquasenseV2.jpg", width: 500, height: 500 },
-    { href: "/images/aquasenseV3.jpg", width: 500, height: 500 },
+    { src: aquasenseV1Image, width: 500, height: 500 },
+    { src: aquasenseV2Image, width: 500, height: 500 },
+    { src: aquasenseV3Image, width: 500, height: 500 },
   ];
 
   const nextGenImages = [
-    { href: "/images/nextgenV1.jpg", width: 500, height: 500 },
-    { href: "/images/nextgenV2.jpg", width: 500, height: 500 },
-    { href: "/images/nextgenV3.jpg", width: 500, height: 500 },
-    { href: "/images/nextgenV4.jpg", width: 500, height: 500 },
-    { href: "/images/nextgenV5.jpg", width: 500, height: 500 },
-    { href: "/images/nextgenV6.jpg", width: 500, height: 500 },
+    { src: nextgenV1Image, width: 500, height: 500 },
+    { src: nextgenV2Image, width: 500, height: 500 },
+    { src: nextgenV3Image, width: 500, height: 500 },
+    { src: nextgenV4Image, width: 500, height: 500 },
+    { src: nextgenV5Image, width: 500, height: 500 },
+    { src: nextgenV6Image, width: 500, height: 500 },
   ];
 
   const rocImages = [
-    { href: "/images/rocV1.jpg", width: 500, height: 500 },
-    { href: "/images/rocV2.jpg", width: 500, height: 500 },
+    { src: rocV1Image, width: 500, height: 500 },
+    { src: rocV2Image, width: 500, height: 500 },
   ];
 
   const durabilityFlyerImages = [
-    { href: "/images/flyerDuurzaamheidV1.png", width: 500, height: 500 },
-    { href: "/images/flyerDuurzaamheidV2.png", width: 500, height: 500 },
+    { src: flyerDuurzaamheidV1, width: 500, height: 500 },
+    { src: flyerDuurzaamheidV2, width: 500, height: 500 },
   ];
 
-  const buildAtHomePresentationImages = [{ href: "/images/buildathomeBolognaV2.jpg", width: 500, height: 500 }];
+  const buildAtHomePresentationImages = [{ src: buildathomeBolognaV2, width: 500, height: 500 }];
 
-  const durabilityPosterImages = [{ href: "/images/posterDuurzaamheidV1.png", width: 500, height: 500 }];
+  const durabilityPosterImages = [{ src: posterDuurzaamheidV1, width: 500, height: 500 }];
 
   return (
     <main className="w-full flex justify-center">
-      <Image src={chillOrbV1Image} alt="chill orb v1" />
       <div className="grid grid-cols-2 w-3/5 p-0 gap-8">
         <Card
           text="ChillOrb | As shown on the slides, this is a webshop selling a brand new innovative (mockup) product that helps you relax due to colour therapy. The product is a smart orb-like object that changes colour to set the mood as well as playing relaxing music. The web design choices like using a darkened background against bright spotlights are made to mirror the overall vibe of the product."
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Image from "next/image";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Image, { StaticImageData } from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 type ImageProps = {
-  href: string;
+  src: StaticImageData;
   width: number;
   height: number;
 };
@@ -17,16 +17,16 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ text, images }) => {
   return (
     <div className="shadow-md rounded-md h-full w-full bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100">
-      <Carousel showThumbs={false} infiniteLoop >
+      <Carousel showThumbs={false} infiniteLoop>
         {images.map((image, index) => (
           <div key={index}>
             <Image
               width={image.width}
               height={image.height}
-              src={image.href}
+              src={image.src}
               alt={`Slide ${index + 1}`}
               style={{
-                borderRadius:"0.375rem 0.375rem 0 0",
+                borderRadius: "0.375rem 0.375rem 0 0",
                 overflow: "hidden",
               }}
             />
